896044a70ec17f4a13c7444e8dc5cf69979a97a3
WJXXBSH <zyb_1998@outlook.com>
2019-02-25 17:22:01 +0800

    Remove props for volume steps

--- system_prop.mk
+++ system_prop.mk
@@ -196,9 +196,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.audio.sdk.fluencetype=fluence \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.fluence.speaker=true \
-    ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    persist.vendor.audio.fluence.speaker=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.sensors.gestures=true \
